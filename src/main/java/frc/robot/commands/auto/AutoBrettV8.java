0d87e08620b580ea6e493b915a7782ee74b461db
StaleyRobotics <staleyrobotics@gmail.com>
2021-03-25 17:22:38 -0500

    Dustin told me to (PathWeaver additions for auto)

---
+++ src/main/java/frc/robot/commands/auto/AutoBrettV8.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import frc.robot.subsystems.DriveTrain;
+
+public class AutoBrettV8 {
+
+//  Trajectory a_b_MovePastAutoLine = TrajectoryGenerator.generateTrajectory(
+//      driveTrain.getPoseListFromPathWeaverJson("Forward"),
+//      driveTrain.createTrajectoryConfig(true));
+
+  //addCommands
+}
